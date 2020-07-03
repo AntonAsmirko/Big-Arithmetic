31224c84c88e9d997da03a26ec4e0b4ecc244eb8
Anton Asmirko <AntonAsmirko@yandex.ru>
2020-07-03 11:49:54 +0300

    .

--- big_arithmetic.cpp
+++ big_arithmetic.cpp
@@ -1,12 +1,7 @@
 #include <stdio.h>
+#include <string>
 
-size_t str_len(const char *str)
-{
-    size_t len = 0;
-    for (; str[len] != '\0'; len++)
-        ;
-    return len;
-}
+#define NULL_STR (string) "NULL"
 
 class Big_Integer
 {
@@ -15,28 +10,33 @@ private:
     bool is_lower_zero;
     unsigned char *digits;
 
-    void allocate_and_fill(char *num, int untill)
+    Big_Integer(size_t size)
     {
-        this->size = str_len(num);
         this->digits = new unsigned char[size];
-        for (int i = size; i >= untill; i--)
+        this->size = size;
+    }
+
+    void fill(std::string num, int untlill)
+    {
+        for (int i = num.size() - 1; i >= 0; i++)
         {
-            this->digits[size - i - 1] = num[i] - '0';
+            this->digits[this->size - i] = num[i];
         }
     }
 
 public:
-    Big_Integer(char *num)
+    Big_Integer(std::string num) : Big_Integer(num.length())
     {
+
         if (num[0] != '-')
         {
             this->is_lower_zero = false;
-            allocate_and_fill(num, 0);
+            fill(num, 0);
         }
         else
         {
             this->is_lower_zero = true;
-            allocate_and_fill(num, 1);
+            fill(num, 1);
         }
     }
 
@@ -52,10 +52,10 @@ public:
      *  1 if this greater than another
      **/
 
-    int compare_to(const Big_Integer &another)
+    int compare_to(Big_Integer *another)
     {
         int this_size = size;
-        int another_size = another.size;
+        int another_size = another->size;
 
         if (this_size > another_size)
         {
@@ -70,7 +70,7 @@ public:
         }
         else if (this_size < another_size)
         {
-            if (another.is_lower_zero)
+            if (another->is_negative())
             {
                 return 1;
             }
@@ -82,7 +82,7 @@ public:
 
         for (; this_size >= 0; this_size--)
         {
-            if (this->digits[this_size] > another.digits[this_size])
+            if (this->digits[this_size] > another->digits[this_size])
             {
                 if (this->is_negative())
                 {
@@ -93,9 +93,9 @@ public:
                     return 1;
                 }
             }
-            else if (this->digits[this_size] < another.digits[this_size])
+            else if (this->digits[this_size] < another->digits[this_size])
             {
-                if (another.is_lower_zero)
+                if (another->is_negative())
                 {
                     return 1;
                 }
@@ -109,6 +109,40 @@ public:
         return 0;
     }
 
+    bool equals(Big_Integer *another)
+    {
+        return this->compare_to(another) == 0;
+    }
+
+    Big_Integer *add(Big_Integer *another)
+    {
+        Big_Integer *max;
+
+        if (this->compare_to(another) == 1)
+        {
+            max = this;
+        }
+        else
+        {
+            max = another;
+        }
+
+        size_t new_size = max->size;
+
+        if (this->is_negative() && !another->is_negative())
+        {
+        }
+        else if (!this->is_negative() && another->is_negative())
+        {
+        }
+        else if (!this->is_negative() && !another->is_negative())
+        {
+        }
+        else
+        {
+        }
+    }
+
     ~Big_Integer()
     {
         delete[] digits;
@@ -118,6 +152,5 @@ public:
 int main()
 {
     Big_Integer *my_big = new Big_Integer("12345");
-    printf("%d", my_big->is_negative());
     return 0;
 }
